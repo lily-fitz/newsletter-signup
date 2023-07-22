786fa24234c5550344b67f90f3fed58db7b253e9
Lily <lily@444s-Air.attlocal.net>
2023-07-22 02:06:25 -0400

    add dataset

---
+++ database.js
@@ -0,0 +1,16 @@
+const emailList = [
+  {
+    id: 1,
+    email: 'emmabenson82@gmail.com',
+  },
+  {
+    id: 2,
+    email: 'paul_smith@gmail.com',
+  },
+  {
+    id: 3,
+    email: 'ali_gomez97@gmail.com',
+  },
+]
+
+export default emailList
